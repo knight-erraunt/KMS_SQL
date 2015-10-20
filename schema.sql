1d7042fad975397c4c6922820cb5738394c7409a
Szymon Sliwa <knight.erraunt@gmail.com>
2015-10-20 20:11:10 +0200

    Added categories, modified varchar lenght

--- schema.sql
+++ schema.sql
@@ -1,16 +1,26 @@
 
+CREATE TABLE categories (
+    category_name VARCHAR(128) PRIMARY KEY
+);
+
+CREATE TABLE categories_to_archive_files (
+    archive_file_id INTEGER REFERENCES archive_files,
+    category_name VARCHAR(128) REFERENCES categories
+);
 
 CREATE TABLE people (
 	person_id INTEGER PRIMARY KEY,
-	name VARCHAR(20),
-	surname VARCHAR(20)
+	name VARCHAR(128),
+	surname VARCHAR(128)
 );
 
 CREATE TABLE archive_files (
 	archive_file_id INTEGER PRIMARY KEY
-	signature VARCHAR(20),
-	time DATE,
-	description TEXT
+	signature VARCHAR(128),
+    date_lower_bound DATE,
+    date_upper_bound DATE,
+	description TEXT,
+	check (date_lower_bound < date_upper_bound)
 );
 
 CREATE TABLE people_to_archive_files (
