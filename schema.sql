b891fe995a48c44d107363b2e9f42b14629fcf17
Szymon Sliwa <z1104721@31112b7b14d4>
2015-10-20 19:16:21 +0200

    Deleted most of my hard work..

--- schema.sql
+++ schema.sql
@@ -8,32 +8,14 @@ CREATE TABLE people (
 
 CREATE TABLE archive_files (
 	archive_file_id INTEGER PRIMARY KEY
+	signature VARCHAR(20),
+    time DATE,
+    description TEXT
 );
 
 CREATE TABLE people_to_archive_files (
 	person_id INTEGER REFERENCES people,
 	archive_file_id INTEGER REFERENCES archive_files
 );
-
-CREATE TABLE events (
-	event_id INTEGER PRIMARY KEY,
-	event_name VARCHAR(20),
-	event_description TEXT,
-	event_begin TIMESTAMP NOT NULL,
-	event_end TIMESTAMP NOT NULL,
-	CHECK (event_begin < event_end)
-);
-
-CREATE TABLE event_to_files (
-	event_id INTEGER REFERENCES events,
-	archive_file INTEGER REFERENCES archive_files
-);
-
-CREATE TABLE events_people ( -- Do we want to log all people present at an event?
-	event_id INTEGER REFERENCES events,
-	person_id INTEGER REFERENCES people
-);
-
-
 
 
